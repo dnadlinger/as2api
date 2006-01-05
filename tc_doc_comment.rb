85c8ab888a38dd011dab09069d6b6fc65e047581
dave <dave@4f4d65cf-00e9-0310-82ac-fa7a581d2dae>
2006-01-05 21:48:42 +0000

    Update unit tests to account for API changes made a while back (bad Dave)
    git-svn-id: http://svn.badgers-in-foil.co.uk/as2api/trunk/as2api@263 4f4d65cf-00e9-0310-82ac-fa7a581d2dae

--- tc_doc_comment.rb
+++ tc_doc_comment.rb
@@ -3,58 +3,77 @@ require 'doc_comment'
 require 'api_loader'
 
 class TC_DocComment < Test::Unit::TestCase
-  def test_strip_stars()
-    doc = DocComment.new(LocalTypeResolver.new)
-    initial = "foo = a * b\nbar!"
-    with_stars = "\t **#{initial}"
-    assert_equal(initial, doc.strip_stars(with_stars))
-  end
-
   def test_description()
-    doc = DocComment.new(LocalTypeResolver.new)
     text = "foo bar\n *blat\n * @param foo bar\n blat ping pong\n *"
-    doc.parse(text)
-    assert_equal("foo bar\nblat", doc.description)
+    comment_data = parse_it(text)
+    assert_equal("foo bar\nblat", comment_data[0].inlines[0].strip)
   end
 
   def test_params()
-    doc = DocComment.new(LocalTypeResolver.new)
     text = "*\n * @param foo bar\n blat ping pong\n *"
-    doc.parse(text)
-    assert_equal("bar\nblat ping pong", doc.param("foo"))
+    comment_data = parse_it(text)
+    expected = ParamBlockTag.new();
+    expected.param_name = "foo"
+    expected.add_inline("bar\n blat ping pong\n")
+    assert_equal(expected, comment_data[1])
   end
 
   def test_return()
-    doc = DocComment.new(LocalTypeResolver.new)
     text = "*\n * @return foo bar\n blat\n *"
-    doc.parse(text)
-    assert_equal("foo bar\nblat", doc.desc_return)
+    comment_data = parse_it(text)
+    expected = ReturnBlockTag.new();
+    expected.add_inline(" foo bar\n blat\n")
+    assert_equal(expected, comment_data[1])
   end
 
   def test_see()
-    doc = DocComment.new(LocalTypeResolver.new)
     text = "*\n * @see foo bar\n blat\n *"
-    doc.parse(text)
-    expected = "foo bar\nblat"
-    assert(doc.seealso_a.member?(expected), "@see didn't have #{expected.inspect}")
+    comment_data = parse_it(text)
+    expected = BlockTag.new
+    expected.add_inline("\n ")
+    assert_equal(expected, comment_data[0])
+    expected = SeeBlockTag.new
+    expected.add_inline("foo bar\nblat")
+    assert_equal(expected, comment_data[1])
+    #assert(doc.seealso_a.member?(expected), "@see didn't have #{expected.inspect}")
   end
 
   def test_throws()
-    doc = DocComment.new(LocalTypeResolver.new)
     text = "*\n * @throws foo.Bbar blat\nping\n *"
-    doc.parse(text)
-    assert("blat\nping", doc.describe_exception("foo.Bar"))
+    comment_data = parse_it(text)
+    expected = ThrowsBlockTag.new();
+    expected.add_inline("blat\nping")
+    assert(expected, comment_data[1])
   end
 
-  def test_package_html
-    [
-      "<html><body>test</body></html>",
-      "<body>te<b>st</b></body>"
-    ].each do |text|
-      PackageHTML.process(text) do |element|
-	a = REXML::XPath.match(element, "descendant::text()").join
-        assert_equal("test", a)
-      end
-    end
+#  def test_package_html
+#    [
+#      "<html><body>test</body></html>",
+#      "<body>te<b>st</b></body>"
+#    ].each do |text|
+#      PackageHTML.process(text) do |element|
+#	a = REXML::XPath.match(element, "descendant::text()").join
+#        assert_equal("test", a)
+#      end
+#    end
+#  end
+
+  def parse_it(text)
+    comment_data = CommentData.new
+
+    input = StringIO.new(text)
+    input.lineno = 1
+    lexer = ActionScript::ParseDoc::DocCommentLexer.new(input)
+    lexer.source = caller.last
+    parser = ActionScript::ParseDoc::DocCommentParser.new(lexer)
+    parse_conf_build = ConfigBuilder.new
+    config = parse_conf_build.build_method_config
+    type_resolver = LocalTypeResolver.new(nil)
+    handler = OurDocCommentHandler.new(comment_data, config, type_resolver)
+    parser.handler = handler
+
+    parser.parse_comment
+
+    comment_data
   end
 end
