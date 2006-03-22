c277d50ccd58e0b68920f5ae04501e6b34778e36
dave <dave@4f4d65cf-00e9-0310-82ac-fa7a581d2dae>
2006-03-22 20:47:49 +0000

    Updated license text, per bug 35590.  Added copyright message in new README file
    git-svn-id: http://svn.badgers-in-foil.co.uk/as2api/trunk/as2api@323 4f4d65cf-00e9-0310-82ac-fa7a581d2dae

--- parse/doccomment_parser.rb
+++ parse/doccomment_parser.rb
@@ -1,9 +1,19 @@
 # 
 # Part of as2api - http://www.badgers-in-foil.co.uk/projects/as2api/
 #
-# Copyright (c) 2006 David Holroyd, and contributors.
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
 #
-# See the file 'COPYING' for terms of use of this code.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
 
